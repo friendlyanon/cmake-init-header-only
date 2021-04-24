46f22d32e5b25c8e9d9ceb811f6953ecce2a1ab8
friendlyanon <friendlyanon@users.noreply.github.com>
2021-04-24 22:36:55 +0200

    Update to v0.6.0

--- cmake/headeronlyConfig.cmake
+++ cmake/headeronlyConfig.cmake
@@ -1,1 +1,1 @@
-include(${CMAKE_CURRENT_LIST_DIR}/headeronlyTargets.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/headeronlyTargets.cmake")
--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,5 +1,9 @@
 # ---- Developer mode ----
 
+# Developer mode enables targets and code paths in the CMake scripts that are
+# only relevant for the developer(s) of headeronly
+# Targets necessary to build the project must be provided unconditionally, so
+# consumers can trivially build and package the project
 if(PROJECT_IS_TOP_LEVEL)
   option(headeronly_DEVELOPER_MODE "Enable developer mode" OFF)
 
@@ -10,14 +14,19 @@ endif()
 
 # ---- Warning guard ----
 
+# target_include_directories with the SYSTEM modifier will request the compiler
+# to omit warnings from the provided paths, if the compiler supports that
+# This is to provide a user experience similar to find_package when
+# add_subdirectory or FetchContent is used to consume this project
+set(headeronly_warning_guard "")
 if(NOT PROJECT_IS_TOP_LEVEL)
   option(
-      headeronly_INCLUDE_WITHOUT_SYSTEM
-      "Enable headeronly's warnings for dependents"
-      OFF
+      headeronly_INCLUDES_WITH_SYSTEM
+      "Use SYSTEM modifier for headeronly's includes, disabling warnings"
+      ON
   )
-endif()
-set(headeronly_warning_guard SYSTEM)
-if(PROJECT_IS_TOP_LEVEL OR headeronly_INCLUDE_WITHOUT_SYSTEM)
-  set(headeronly_warning_guard "")
+  mark_as_advanced(headeronly_INCLUDES_WITH_SYSTEM)
+  if(headeronly_INCLUDES_WITH_SYSTEM)
+    set(headeronly_warning_guard SYSTEM)
+  endif()
 endif()

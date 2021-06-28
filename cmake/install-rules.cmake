78caea098db848c300dfe5808b7231e71da30d04
friendlyanon <friendlyanon@users.noreply.github.com>
2021-06-28 20:48:29 +0200

    Update to v0.19.1

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -2,8 +2,14 @@ if(PROJECT_IS_TOP_LEVEL)
   set(CMAKE_INSTALL_INCLUDEDIR include/headeronly CACHE PATH "")
 endif()
 
+# Project is configured with no languages, so tell GNUInstallDirs the lib dir
+set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "")
+
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
+
+# find_package(<package>) call for consumers to find this project
+set(package headeronly)
 
 install(
     DIRECTORY include/
@@ -18,22 +24,27 @@ install(
 )
 
 write_basic_package_version_file(
-    headeronlyConfigVersion.cmake
+    "${package}ConfigVersion.cmake"
     COMPATIBILITY SameMajorVersion
     ARCH_INDEPENDENT
 )
 
 # Allow package maintainers to freely override the path for the configs
 set(
-    headeronly_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/headeronly"
+    headeronly_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/${package}"
     CACHE STRING "CMake package config location relative to the install prefix"
 )
 mark_as_advanced(headeronly_INSTALL_CMAKEDIR)
 
 install(
-    FILES
-    cmake/headeronlyConfig.cmake
-    "${PROJECT_BINARY_DIR}/headeronlyConfigVersion.cmake"
+    FILES cmake/install-config.cmake
+    DESTINATION "${headeronly_INSTALL_CMAKEDIR}"
+    RENAME "${package}Config.cmake"
+    COMPONENT headeronly_Development
+)
+
+install(
+    FILES "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
     DESTINATION "${headeronly_INSTALL_CMAKEDIR}"
     COMPONENT headeronly_Development
 )

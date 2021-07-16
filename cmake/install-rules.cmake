6515a7e7ab2ab73714ac486c86acb6f406926bf6
friendlyanon <friendlyanon@users.noreply.github.com>
2021-07-16 04:23:29 +0200

    Update to v0.20.0

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -31,8 +31,8 @@ write_basic_package_version_file(
 
 # Allow package maintainers to freely override the path for the configs
 set(
-    headeronly_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/${package}"
-    CACHE STRING "CMake package config location relative to the install prefix"
+    headeronly_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/${package}"
+    CACHE PATH "CMake package config location relative to the install prefix"
 )
 mark_as_advanced(headeronly_INSTALL_CMAKEDIR)
 
